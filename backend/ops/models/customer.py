31da1acfcecd47d8a8ef8fdc4595caa8d887733b
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-19 07:17:04 +0900

    ops customer define (#6)
    * test(ops): Customer model を定義
    
    * feat(ops): Customer model を実装

---
+++ backend/ops/models/customer.py
@@ -0,0 +1,30 @@
+# backend/ops/models/customer.py
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+    def clean(self):
+        super().clean()
+
+        errors = {}
+
+
+        
+        if self.name is None or self.name.strip() == "":
+            errors["name"] = "name を空白にはできません。"
+
+
+
+        if errors:
+            raise ValidationError(errors)
