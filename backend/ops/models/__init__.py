f55100c03646915158545ec45e2a2f105bab95aa
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-19 19:06:54 +0900

    feat(ops): Task model を実装

--- backend/ops/models/__init__.py
+++ backend/ops/models/__init__.py
@@ -7,3 +7,4 @@ from .department_item_assignment_component import DepartmentItemAssignmentCompon
 from .order_header import OrderHeader
 from .order_line import OrderLine
 from .task_key import TaskKey
+from .task import Task
