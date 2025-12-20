8135882bcee21a9fbc2412d4be6c6ab2b9a1fe93
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-21 07:58:20 +0900

    ops task derive two level task (#21)
    * test(ops): OrderLine から 2 段目の Task を導出できるか
    
    * feat(ops): OrderLine から 2 段目の Task を導出

--- backend/ops/tests/services/test_task_deriver.py
+++ backend/ops/tests/services/test_task_deriver.py
@@ -101,3 +101,64 @@ def test_derive_tasks_expands_components_one_level():
     assert Task.objects.get(assignment=asg_base, pickup_at=pickup_at).required_units == 3
     
     assert Task.objects.get(assignment=asg_lettuce, pickup_at=pickup_at).required_units == 6
+
+
+
+@pytest.mark.django_db
+def test_derive_tasks_expands_components_two_levels():
+    # Arrange
+    dept_line = Department.objects.create(name="ライン班")
+    dept_pack = Department.objects.create(name="梱包班")
+    dept_potato = Department.objects.create(name="いも班")
+
+    item_set = Item.objects.create(name="ポテトサラダセット", pack_g=1000)
+    item_base = Item.objects.create(name="ポテトサラダベース", pack_g=800)
+    item_potato_cut = Item.objects.create(name="じゃがいも (乱切り)", pack_g=550)
+
+
+
+    asg_set = DepartmentItemAssignment.objects.create(department=dept_line, item=item_set)
+    asg_base = DepartmentItemAssignment.objects.create(department=dept_pack, item=item_base)
+    asg_potato = DepartmentItemAssignment.objects.create(department=dept_potato, item=item_potato_cut)
+
+
+
+    # set -> base (1)
+    DepartmentItemAssignmentComponent.objects.create\
+    (
+        parent_department_item_assignment=asg_set,
+        child_department_item_assignment=asg_base,
+        child_units_per_parent_unit=1,
+    )
+
+    # base -> potato (1)
+    DepartmentItemAssignmentComponent.objects.create\
+    (
+        parent_department_item_assignment=asg_base,
+        child_department_item_assignment=asg_potato,
+        child_units_per_parent_unit=1,
+    )
+
+
+
+    customer = Customer.objects.create(name="オークワ 田原本店")
+
+    pickup_at = timezone.now()
+
+    header = OrderHeader.objects.create(customer=customer, ordered_at=pickup_at, pickup_at=pickup_at)
+
+    OrderLine.objects.create(order_header=header, product_item=item_set, quantity_units=3)
+
+
+
+    # Act
+    derive_tasks_for_order_header(order_header_id=header.id)
+
+
+
+    # Assert
+    assert Task.objects.get(assignment=asg_set, pickup_at=pickup_at).required_units == 3
+
+    assert Task.objects.get(assignment=asg_base, pickup_at=pickup_at).required_units == 3
+
+    assert Task.objects.get(assignment=asg_potato, pickup_at=pickup_at).required_units == 3
