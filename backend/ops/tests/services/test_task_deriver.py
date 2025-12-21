61882eb1afba53d8854f9cc4f8f007aa550d6f8b
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-21 18:02:10 +0900

    test(ops): OrderLine から全ての Task を導出できるか

--- backend/ops/tests/services/test_task_deriver.py
+++ backend/ops/tests/services/test_task_deriver.py
@@ -162,3 +162,76 @@ def test_derive_tasks_expands_components_two_levels():
     assert Task.objects.get(assignment=asg_base, pickup_at=pickup_at).required_units == 3
 
     assert Task.objects.get(assignment=asg_potato, pickup_at=pickup_at).required_units == 3
+
+
+
+
+@pytest.mark.django_db
+def test_derive_tasks_expands_components_any_depth():
+    # Arrange
+    dept_line = Department.objects.create(name="ライン班")
+    dept_pack = Department.objects.create(name="梱包班")
+    dept_potato = Department.objects.create(name="いも班")
+
+    item_set = Item.objects.create(name="ポテトサラダセット", pack_g=1000)
+    item_base = Item.objects.create(name="ポテトサラダベース", pack_g=800)
+    item_mash = Item.objects.create(name="じゃがいも (マッシュ)", pack_g=550)
+    item_cut = Item.objects.create(name="じゃがいも (乱切り)", pack_g=550)
+
+
+
+    asg_set = DepartmentItemAssignment.objects.create(department=dept_line, item=item_set)
+    asg_base = DepartmentItemAssignment.objects.create(department=dept_pack, item=item_base)
+    asg_mash = DepartmentItemAssignment.objects.create(department=dept_pack, item=item_mash)
+    asg_cut = DepartmentItemAssignment.objects.create(department=dept_potato, item=item_cut)
+
+
+
+    # 3 段以上
+    # set -> base -> mash -> cut
+    DepartmentItemAssignmentComponent.objects.create\
+    (
+        parent_department_item_assignment=asg_set,
+        child_department_item_assignment=asg_base,
+        child_units_per_parent_unit=1,
+    )
+
+    DepartmentItemAssignmentComponent.objects.create\
+    (
+        parent_department_item_assignment=asg_base,
+        child_department_item_assignment=asg_mash,
+        child_units_per_parent_unit=1,
+    )
+
+    DepartmentItemAssignmentComponent.objects.create\
+    (
+        parent_department_item_assignment=asg_mash,
+        child_department_item_assignment=asg_cut,
+        child_units_per_parent_unit=1,
+    )
+
+
+
+    customer = Customer.objects.create(name="オークワ 田原本店")
+
+    pickup_at = timezone.now()
+
+    header = OrderHeader.objects.create(customer=customer, ordered_at=pickup_at, pickup_at=pickup_at)
+
+    OrderLine.objects.create(order_header=header, product_item=item_set, quantity_units=5)
+
+
+
+    # Act
+    derive_tasks_for_order_header(order_header_id=header.id)
+
+
+
+    # Assert
+    assert Task.objects.get(assignment=asg_set, pickup_at=pickup_at).required_units == 5
+
+    assert Task.objects.get(assignment=asg_base, pickup_at=pickup_at).required_units == 5
+
+    assert Task.objects.get(assignment=asg_mash, pickup_at=pickup_at).required_units == 5
+
+    assert Task.objects.get(assignment=asg_cut, pickup_at=pickup_at).required_units == 5
